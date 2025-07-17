b080a49c6df5da5dc0c101a703e788f9caeffe54
ThundaHorse <abeeeeekim@gmail.com>
2025-07-17 03:30:44 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  env: {
-    CAPTCHA_LEGACY_SECRET_KEY: process.env.CAPTCHA_LEGACY_SECRET_KEY,
-    CAPTCHA_SERVER_KEY: process.env.CAPTCHA_SERVER_KEY,
-    CAPTCHA_SITE_KEY: process.env.CAPTCHA_SITE_KEY,
-    NETLIFY_EMAILS_DIRECTORY: process.env.NETLIFY_EMAILS_DIRECTORY,
-    NETLIFY_EMAILS_SECRET: process.env.NETLIFY_EMAILS_SECRET
-  }
+  /* config options here */
+  output: 'standalone'
 };
 
 export default nextConfig;
